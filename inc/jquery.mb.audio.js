1e8a7567df5cf87dcbb215bba2642c277177dac7
pupunzi <matteo@open-lab.com>
2010-06-09 23:29:10 +0200

    1.9.2 -> added description to images

---
+++ inc/jquery.mb.audio.js
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ jquery.mb.components
+ Copyright (c) 2001-2010. Matteo Bicocchi (Pupunzi); Open lab srl, Firenze - Italy
+ email: info@pupunzi.com
+ site: http://pupunzi.com
+
+ Licences: MIT, GPL
+ http://www.opensource.org/licenses/mit-license.php
+ http://www.gnu.org/licenses/gpl.html
+ ******************************************************************************/
+
+/*
+ * jQuery.mb.components: jquery.mb.audio
+ * version: 0.1- 9-giu-2010 - 28
+ * © 2001 - 2010 Matteo Bicocchi (pupunzi), Open Lab
+ */
